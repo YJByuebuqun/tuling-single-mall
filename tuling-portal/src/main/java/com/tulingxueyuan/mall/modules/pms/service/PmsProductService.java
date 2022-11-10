fc24a666e5c82eb0d7bcc7c920652b7a8961f061
yanjiabo <15669506620@163.com>
2022-11-10 14:56:04 +0800

    tuling-mall-02嘎嘎香

--- tuling-portal/src/main/java/com/tulingxueyuan/mall/modules/pms/service/PmsProductService.java
+++ tuling-portal/src/main/java/com/tulingxueyuan/mall/modules/pms/service/PmsProductService.java
@@ -16,6 +16,7 @@ public interface PmsProductService extends IService<PmsProduct> {
     /**
      * /product/detail/${this.id}
      * 描述：展示sku和spu信息
+     * 嘎嘎香
      */
     ProductDetailDTO getProductDetail(Long id);
 }
